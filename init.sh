249e848a1464f27fafc4f9918834cbbb4272d5c5
zaquest <web.elektro.net@gmail.com>
2018-10-04 12:12:51 +0700

    bin/{term,diff}, Xdefaults -> Xresources, clone vundle.

--- init.sh
+++ init.sh
@@ -4,16 +4,19 @@ base=$(dirname $0)
 base=$(readlink -f $base)
 
 ln -sf $base/_xprofile $HOME/.xprofile
-ln -sf $base/_Xdefaults $HOME/.Xdefaults
+ln -sf $base/_Xresources $HOME/.Xresources
 ln -sf $base/_tmux.conf $HOME/.tmux.conf
-ln -sf $base/_zsh $HOME/.zsh
+ln -sfT $base/_zsh $HOME/.zsh
 ln -sf $base/_zshrc $HOME/.zshrc
 ln -sf $base/_dircolors $HOME/.dircolors
 ln -sf $base/_gitconfig $HOME/.gitconfig
 ln -sf $base/_gtkrc-2.0 $HOME/.gtkrc-2.0
 ln -sf $base/_pathrc $HOME/.pathrc
-mkdir -p $HOME/.config/nvim
+mkdir -p $HOME/.config/nvim/bundle
 ln -sf $base/neo.vim $HOME/.config/nvim/init.vim
+if [[ ! -d "$HOME/.config/nvim/bundle/Vundle.vim" ]]; then
+  git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.config/nvim/bundle/Vundle.vim
+fi
 mkdir -p $HOME/.config/fontconfig
 ln -sf $base/fonts.conf $HOME/.config/fontconfig/
 mkdir -p $HOME/.themes
@@ -22,5 +25,5 @@ tar xjf $base/gtk2-awesome.tar.bz2 -C $HOME/.themes
 
 bin=$HOME/.local/bin
 mkdir -p $bin
-
-ln -sf $base/bin/tmux_fmtpanetitle $bin/
+ln -sf $base/bin/term $bin/term
+ln -sf $base/bin/diff $bin/diff
