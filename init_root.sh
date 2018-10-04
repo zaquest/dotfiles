4ad340e668884b80020e973afb92ff0f9a03ca4b
zaquest <web.elektro.net@gmail.com>
2018-10-04 12:27:15 +0700

    root setup

---
+++ init_root.sh
@@ -0,0 +1,12 @@
+base=$(dirname $0)
+base=$(readlink -f $base)
+zaquest=$(eval echo "~zaquest")
+
+mkdir -p $HOME/.config
+ln -sfT $zaquest/.config/nvim $HOME/.config/nvim
+ln -sf $zaquest/.dircolors $HOME/.dircolors
+
+mkdir -p $HOME/.zsh
+cp $base/_zsh/spectrum $HOME/.zsh
+cp $base/_zsh/prompt_root_setup $HOME/.zsh
+cp $base/_zshrc_root $HOME/.zshrc
